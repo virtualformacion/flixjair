d1a66c1ffcd75aa646550e723e96eb58f6778839
virtualformacion <virtualformacion47@gmail.com>
2025-02-28 16:14:02 +0000

    script.js

--- script.js
+++ script.js
@@ -7,11 +7,26 @@ document.getElementById("emailForm").addEventListener("submit", async function(e
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
     });
-    
+
     const data = await response.json();
-    if (data.link) {
+
+    // Si encontramos un enlace de Disney+
+    if (data.alert) {
+        // Mostrar el cuerpo del mensaje de Disney+ en el modal
+        document.getElementById("messageBody").innerHTML = data.body; // Insertar el HTML del cuerpo
+        document.getElementById("messageModal").style.display = 'block'; // Mostrar el modal
+    } 
+    // Si encontramos un enlace de Netflix
+    else if (data.link) {
         window.location.href = data.link; // Redirige automáticamente
-    } else {
+    } 
+    // Si no se encuentra nada
+    else {
         alert("No se encontró resultado para tu cuenta, vuelve a intentarlo nuevamente.");
     }
+});
+
+// Función para cerrar el modal
+document.getElementById("closeModal").addEventListener("click", function() {
+    document.getElementById("messageModal").style.display = 'none'; // Ocultar el modal
 });
